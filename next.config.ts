15c036da646b77e28ed263c6998e89073972c4a2
Darksister22 <fatema@samaursoft.com>
2026-07-14 19:31:24 +0300

    config issues

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,16 @@
 import type { NextConfig } from "next";
+const supabaseHost = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL!).hostname;
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-module.exports = {
-  allowedDevOrigins: ['26.16.152.149','192.168.56.1','192.168.68.112','172.20.10.2'],
-   images: {
+  allowedDevOrigins: ['26.16.152.149', '192.168.56.1', '192.168.68.112', '172.20.10.2'],
+  images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "yemsordohjhlsymaihua.supabase.co",
+        hostname: supabaseHost,
         pathname: "/storage/v1/object/public/**",
-      },
-    ],
-  },
-}
+      }
+    ]
+  }
+};
+export default nextConfig;
